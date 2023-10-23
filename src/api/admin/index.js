9056649e42b0c1c4ec30b6188d43a7c783dbf4fc
diogocerutti <diogo.cerutti19@gmail.com>
2023-10-23 17:52:10 -0300

    Admin API routes

---
+++ src/api/admin/index.js
@@ -0,0 +1,38 @@
+import { api } from "../index.js";
+
+import { api } from "..";
+
+export async function getAllAdmins() {
+  try {
+    const response = await api.get("/admin");
+    if (response.status === 200) {
+      return response.data;
+    }
+  } catch (error) {
+    console.log(error);
+    return [];
+  }
+}
+
+export async function loginAdmin(request) {
+  const body = await request;
+
+  const { email, password } = body;
+
+  try {
+    const response = await api.post(
+      "/admin/login",
+      {
+        email: email,
+        password: password,
+      },
+      { withCredentials: true }
+    );
+    if (response.status === 200) {
+      return response.data;
+    }
+  } catch (error) {
+    // mensagem de erro do back
+    return error;
+  }
+}
