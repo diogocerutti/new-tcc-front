6e5a8b58acbeb264a40c43664e52f2089f34ac48
diogocerutti <diogo.cerutti19@gmail.com>
2023-10-23 23:29:25 -0300

    NotFound Page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import { AdminLoginPage } from "./pages/AdminLogin";
-import { HomePage } from "./pages/Home/index.js";
+import { AdminDashboardPage } from "./pages/AdminDashboard";
+import { HomePage } from "./pages/Home";
+import { NotFound } from "./pages/NotFound/index.js";
 import { ProtectRoutes } from "./hooks/protectRoutes/index.js";
 import { Route, Routes, Navigate } from "react-router-dom";
 
@@ -8,9 +10,11 @@ function App() {
     <Routes>
       <Route path="/" element={<Navigate to="home" exact />} />
       <Route path="/admin/login" element={<AdminLoginPage />} />
+      <Route path="*" element={<NotFound />} />
 
       <Route element={<ProtectRoutes />}>
         <Route path="/home" element={<HomePage />} />
+        <Route path="/admin/dashboard" element={<AdminDashboardPage />} />
       </Route>
     </Routes>
   );
