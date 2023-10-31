d7ae9dd4e1190812187c5f6d4e2e4242bd1eb66c
diogocerutti <diogo.cerutti19@gmail.com>
2023-10-31 11:34:50 -0300

    First Options Bar

--- src/components/AdminTable/index.js
+++ src/components/AdminTable/index.js
@@ -1,11 +1,14 @@
-import * as React from "react";
-import Table from "@mui/material/Table";
-import TableBody from "@mui/material/TableBody";
-import TableCell from "@mui/material/TableCell";
-import TableContainer from "@mui/material/TableContainer";
-import TableHead from "@mui/material/TableHead";
-import TableRow from "@mui/material/TableRow";
-import Paper from "@mui/material/Paper";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Paper,
+  Grid,
+} from "@mui/material";
+import AddCircleIcon from "@mui/icons-material/AddCircle";
 import { getAllProducts } from "../../api/product";
 import { useState, useCallback, useEffect } from "react";
 
@@ -22,46 +25,57 @@ export default function AdminTable() {
   }, [handleGetProducts]);
 
   return (
-    <TableContainer component={Paper}>
-      <Table sx={{ minWidth: 650 }} aria-label="simple table">
-        <TableHead>
-          <TableRow>
-            <TableCell>ID</TableCell>
-            <TableCell align="right">Nome</TableCell>
-            <TableCell align="right">Preço</TableCell>
-            <TableCell align="right">Unidade Medida</TableCell>
-            <TableCell align="right">Descrição</TableCell>
-            <TableCell align="right">Categoria</TableCell>
-            <TableCell align="right">Imagem</TableCell>
-            <TableCell align="right">Opções</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {products.map((row) => (
-            <TableRow
-              key={row.id}
-              sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
-            >
-              <TableCell component="th" scope="row">
-                {row.id}
-              </TableCell>
-              <TableCell align="right">{row.name}</TableCell>
-              <TableCell align="right">{row.price}</TableCell>
-              <TableCell align="right">{row.measure}</TableCell>
-              <TableCell align="right">...</TableCell>
-              <TableCell align="right">{row.category}</TableCell>
-              <TableCell align="right">
-                <img
-                  alt="imagem"
-                  src={require(`C:/Users/diogo/Desktop/Produtos/${row.image}`)}
-                  style={{ height: "1.6vw", width: "1.6vw" }}
-                />
-              </TableCell>
+    <>
+      <Grid container justifyContent={"space-around"}>
+        <Grid item>Usuários</Grid>
+        <Grid item>Usuários Administradores</Grid>
+        <Grid item>Produtos</Grid>
+        <Grid item>Pedidos</Grid>
+        <Grid item>
+          <AddCircleIcon color="success" />
+        </Grid>
+      </Grid>
+      <TableContainer component={Paper}>
+        <Table sx={{ minWidth: 650 }} aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell>ID</TableCell>
+              <TableCell align="right">Nome</TableCell>
+              <TableCell align="right">Preço</TableCell>
+              <TableCell align="right">Unidade Medida</TableCell>
+              <TableCell align="right">Descrição</TableCell>
+              <TableCell align="right">Categoria</TableCell>
+              <TableCell align="right">Imagem</TableCell>
               <TableCell align="right">Opções</TableCell>
             </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
+          </TableHead>
+          <TableBody>
+            {products.map((row) => (
+              <TableRow
+                key={row.id}
+                sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+              >
+                <TableCell component="th" scope="row">
+                  {row.id}
+                </TableCell>
+                <TableCell align="right">{row.name}</TableCell>
+                <TableCell align="right">{row.price}</TableCell>
+                <TableCell align="right">{row.measure}</TableCell>
+                <TableCell align="right">...</TableCell>
+                <TableCell align="right">{row.category}</TableCell>
+                <TableCell align="right">
+                  <img
+                    alt="imagem"
+                    src={require(`C:/Users/diogo/Desktop/Produtos/${row.image}`)}
+                    style={{ height: "1.6vw", width: "1.6vw" }}
+                  />
+                </TableCell>
+                <TableCell align="right">Opções</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </>
   );
 }
