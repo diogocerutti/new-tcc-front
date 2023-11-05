19d63e7605e01eaca2fed5f714e127f6fd4fdefb
diogocerutti <diogo.cerutti19@gmail.com>
2023-11-05 20:21:00 -0300

    Admin Sidebar

--- src/hooks/protectRoutes/index.js
+++ src/hooks/protectRoutes/index.js
@@ -1,4 +1,5 @@
 import { Outlet, Navigate } from "react-router-dom";
+import { AdminSidebar } from "../../components/AdminSidebar";
 /* import { useAuth } from "../../api/admin"; */
 import Cookies from "js-cookie";
 
@@ -6,5 +7,11 @@ export const ProtectRoutes = () => {
   /* const { cookies } = useAuth(); */
   const cookie = Cookies.get("admin");
 
-  return cookie ? <Outlet /> : <Navigate to="/admin/login" exact />; // se "cookies.admin" existir, vá para Outlet, se não, vá para /admin/login
+  return cookie ? (
+    <AdminSidebar>
+      <Outlet />
+    </AdminSidebar>
+  ) : (
+    <Navigate to="/admin/login" exact />
+  ); // se "cookies.admin" existir, vá para Outlet, se não, vá para /admin/login
 };
