640122ec5c0eb58dd1ff3482c25dfaebc809d949
diogocerutti <diogo.cerutti19@gmail.com>
2023-11-27 23:40:08 -0300

    MeasureType API

--- src/api/measure_type/index.js
+++ src/api/measure_type/index.js
@@ -11,3 +11,51 @@ export async function getAllMeasures() {
     return [];
   }
 }
+
+export async function getOneMeasure(id) {
+  try {
+    const response = await api.get(`/measure_type/${id}`);
+    if (response.status === 200) {
+      return response.data;
+    }
+  } catch (error) {
+    console.log(error);
+    return [];
+  }
+}
+
+export async function createMeasure(data) {
+  try {
+    const response = await api.post("/measure_type", data);
+    if (response.status === 200) {
+      return response.data;
+    }
+  } catch (error) {
+    console.log("ERRO AO CRIAR UNIDADE DE MEDIDA: ", error);
+    return error;
+  }
+}
+
+export async function updateMeasure(id, data) {
+  try {
+    const response = await api.put(`/measure_type/${id}`, data);
+    if (response.status === 200) {
+      return response.data;
+    }
+  } catch (error) {
+    console.log("ERRO AO EDITAR UNIDADE DE MEDIDA: ", error);
+    return error;
+  }
+}
+
+export async function deleteMeasure(id) {
+  try {
+    const response = await api.delete(`/measure_type/${id}`);
+    if (response.status === 200) {
+      return response.data;
+    }
+  } catch (error) {
+    console.log("ERRO AO EXCLUIR UNIDADE DE MEDIDA: ", error.response.data.msg);
+    return error;
+  }
+}
